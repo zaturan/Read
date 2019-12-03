ed4a8cf386ab79a2f96ec44779b88859e568b9aa
zaturan <farrahmardiana@gmail.com>
2019-12-04 00:58:38 +0800

    Update User.php

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'country','zip', 'city', 'address','phone_number'
     ];
 
     /**
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function users(){
+        return $this->hasMany('App\Book');
+    }
 }
