a3cfa488fa73dabefbb1f209bbeeb1694b6b3db2
zaturan <farrahmardiana@gmail.com>
2019-12-04 00:58:45 +0800

    Update BiddingController.php

--- app/Http/Controllers/BiddingController.php
+++ app/Http/Controllers/BiddingController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Bid;
+use App\Book;
+use App\Author;
+use App\Genre;
+
 class BiddingController extends Controller
 {
     /**
