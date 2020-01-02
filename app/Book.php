576ce0eb4422ec8580ab1407a0f8a7f2124612d4
zaturan <farrahmardiana@gmail.com>
2020-01-02 16:36:29 +0800

    Update Book.php

--- app/Book.php
+++ app/Book.php
@@ -8,7 +8,7 @@ class Book extends Model
 {
     //
     protected $table='books';
-    protected $fillable = ['user_id','img', 'title', 'aut_id', 'genre_id','desc', 'year', 'min_price', 'max_price', 'buyout_price', 'end_date', 'status'];
+    protected $fillable = ['user_id','img', 'title', 'author', 'genre_id','desc', 'year', 'min_price', 'max_price', 'buyout_price', 'end_date', 'status'];
 
     // public function authors(){
     //     return $this->hasMany('App\Author');
@@ -19,10 +19,10 @@ class Book extends Model
         return $this->belongsTo('App\Genre', 'genre_id');
     }
 
-    public function authors()
-    {
-        return $this->belongsTo('App\Author', 'aut_id');
-    }
+    // public function authors()
+    // {
+    //     return $this->belongsTo('App\Author', 'aut_id');
+    // }
 
     public function users(){
         return $this->belongsTo('App\User', 'user_id');
