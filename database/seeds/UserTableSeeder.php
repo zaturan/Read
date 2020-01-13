4ee7e3bf8676001cd90224d57bb423ca0d0049ce
zaturan <farrahmardiana@gmail.com>
2020-01-13 11:58:59 +0800

    enter

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -13,11 +13,6 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        $admin = new User();
-        $admin->name = 'admin Name';
-        $admin->email = 'letsread76@gmail.com';
-        $admin->password = bcrypt('Letsread2019');
-        $admin->save();
-        $admin->roles()->attach($role_admin);
+       //
     }
 }
